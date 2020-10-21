d50d7483bd94197d01b5d4b38832ebcbc6ebabe1
charlie retzler <charlie.retzler@gmail.com>
2020-10-21 19:57:52 -0400

    Monty Hall Game

---
+++ src/C#/SolutionTemplate/Project/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    public class Game
+    {
+        public List<Door> Doors { get; }
+
+        public Game()
+        {
+            // Generate prize in one of three doors
+            this.Doors = new List<Door>() {new Door(), new Door(), new Door()};
+
+            var rand = new Random();
+            var carIndex = rand.Next(0, 3);
+
+            for (var current = 0; current < this.Doors.Count; current++)
+            {
+                if (current == carIndex)
+                {
+                    this.Doors[current].IsCar = true;
+                }
+                else
+                {
+                    this.Doors[current].IsCar = false;
+                }
+            }
+        }
+
+        public int ShowUnchosenGoat(int playersChoice)
+        {
+            for (var index = 0; index < this.Doors.Count; index++)
+            {
+                if ((index != playersChoice) && this.Doors[index].IsGoat)
+                {
+                    return index;
+                }
+            }
+
+            throw new Exception("All Cars");
+        }
+    }
+
+    public class Door
+    {
+        public bool IsCar { get; set; }
+
+        public bool IsGoat
+        {
+            get { return this.IsCar == false; }
+        }
+    }
+
+    public class AutomaticPlayer
+    {
+        public int ChooseDoor()
+        {
+            var rand = new Random();
+            var choiceIndex = rand.Next(0, 3);
+            return choiceIndex;
+        }
+
+        public bool Stay { get; set; }
+    }
+}
--- src/C#/SolutionTemplate/Project/Program.cs
+++ src/C#/SolutionTemplate/Project/Program.cs
@@ -6,7 +6,51 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            for (var index = 0; index < 10; index++)
+            {
+                var game = new Game();
+                var player = new AutomaticPlayer();
+                var choice = player.ChooseDoor();
+
+                Console.WriteLine($"Doors[0] {game.Doors[0].IsCar}");
+                Console.WriteLine($"Doors[1] {game.Doors[1].IsCar}");
+                Console.WriteLine($"Doors[2] {game.Doors[2].IsCar}");
+                Console.WriteLine($"Choice {choice}");
+
+                var showGoatDoor = game.ShowUnchosenGoat(choice);
+
+                Console.WriteLine($"Goat Behind Door {showGoatDoor}");
+
+                if (!player.Stay)
+                {
+                    // Change choice to not current and not revealed goat
+                    choice = SwitchChoice(game, choice, showGoatDoor);
+                }
+
+
+                if (game.Doors[choice].IsCar)
+                {
+                    Console.WriteLine("Win");
+                }
+                else
+                {
+                    Console.WriteLine("Loss");
+                }
+            }
+        }
+
+        private static int SwitchChoice(Game game, int choice, int showGoatDoor)
+        {
+            for (var index = 0; index < game.Doors.Count; index++)
+            {
+                if (choice != index && showGoatDoor != index)
+                {
+                    return index;
+                }
+            }
+
+            throw new Exception("Can't switch source");
         }
     }
 }
