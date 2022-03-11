ec84f1fa9f914ff77f2dde98a7aa824d157ccd4d
ken.baum <ilpadre1953@gmail.com>
2022-03-10 20:53:55 -0500

    two characters separated by a space with width of 2 returns the characters separated by a newline

--- Templates/JavaScript/app.js
+++ Templates/JavaScript/app.js
@@ -2,7 +2,7 @@ function wordWrap(s, w) {
     if (w >= s.length) {
         return s
     } else {
-        return s.substring(0, w) + "\n" + wordWrap(s.substring(w).trim(), w)
+        return s.substring(0, w).trim() + "\n" + wordWrap(s.substring(w).trim(), w)
     }
 }
 
