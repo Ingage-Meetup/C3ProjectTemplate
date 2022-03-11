8b6a066e3deb83192353ebf1a780f8b4d779e4b1
ken.baum <ilpadre1953@gmail.com>
2022-03-10 20:22:05 -0500

    two characters with width of 1 returns characters separated by newline

--- Templates/JavaScript/app.js
+++ Templates/JavaScript/app.js
@@ -1,5 +1,9 @@
-function wordWrap(inputString, width) {
-    return inputString
+function wordWrap(s, w) {
+    if (w >= s.length) {
+        return s
+    } else {
+        return s.substring(0, w) + "\n" + s.substring(w)
+    }
 }
 
 module.exports = { wordWrap };
--- Templates/JavaScript/app.test.js
+++ Templates/JavaScript/app.test.js
@@ -4,10 +4,13 @@ const { wordWrap  } = require("./app")
 
 describe("Wordwrap test", function() {
     it("empty string with width of 1 returns empty string", function() {
-        assert.equal("", wordWrap("", 1))
+        assert.equal(wordWrap("", 1),"" )
     })
     it("single character with width of 1 returns single character", function() {
-        assert.equal("x", wordWrap("x", 1))
+        assert.equal(wordWrap("x", 1), "x")
+    })
+    it("two characters with width of 1 returns characters separated by newline", function() {
+        assert.equal(wordWrap("xx", 1), "x\nx")
     })
 })
         
