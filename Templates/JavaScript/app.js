95b2652958276765ac1d69ae714d3787912886d8
ken.baum <ilpadre1953@gmail.com>
2022-03-15 07:37:54 -0400

    Restore order to the universe

--- Templates/JavaScript/app.js
+++ Templates/JavaScript/app.js
@@ -1,31 +1,10 @@
-function wordWrap(s, w) {
-    if (w >= s.length) {
-        return s
-    } else {
-        let br = s.lastIndexOf(" ", w)
-        if (br === -1) {
-            br = w
-        }
-        return s.substring(0, br).trim() + "\n" + wordWrap(s.substring(br).trim(), w)
-    }
+// Implement your exercise in this file.  If you need to implement additional functions,
+// remember to export them as well, if you need to access them in your unit test.
+function doAllTheThings() {
+    console.log("Hi there");
+    return true;
+function wordWrap() {
+    return ""
 }
 
-let text = "The quick brown fox jumps over the lazy dog"
-
-let wrappedText = wordWrap(text, 5)
-
-console.log(wrappedText)
-
-wrappedText = wordWrap(text, 10)
-
-console.log(wrappedText)
-
-let manInTheArena = "It is not the critic who counts; not the man who points out how the strong man stumbles, or where the doer of deeds could have done them better. The credit belongs to the man who is actually in the arena, whose face is marred by dust and sweat and blood; who strives valiantly; who errs, who comes short again and again, because there is no effort without error and shortcoming; but who does actually strive to do the deeds; who knows great enthusiasms, the great devotions; who spends himself in a worthy cause; who at the best knows in the end the triumph of high achievement, and who at the worst, if he fails, at least fails while daring greatly, so that his place shall never be with those cold and timid souls who neither know victory nor defeat."
-wrappedText = wordWrap(manInTheArena, 80)
-console.log(wrappedText)
-
-
-wrappedText = wordWrap(manInTheArena, 40)
-
-console.log(wrappedText)
-module.exports = { wordWrap };
+module.exports = { doAllTheThings };
--- Templates/JavaScript/app.test.js
+++ Templates/JavaScript/app.test.js
@@ -1,63 +1,6 @@
-const { assert } = require("chai")
-const { wordWrap  } = require("./app")
+const { doAllTheThings } = require('./app');
 
-
-describe("Wordwrap test", function() {
-    it("empty string with width of 1 returns empty string", function() {
-        assert.equal(wordWrap("", 1),"" )
-    })
-    it("single character with width of 1 returns single character", function() {
-        assert.equal(wordWrap("x", 1), "x")
-    })
-    it("two characters with width of 1 returns characters separated by newline", function() {
-        assert.equal(wordWrap("xx", 1), "x\nx")
-    })
-    it("two characters with width of 2 returns  the characters", function() {
-        assert.equal(wordWrap("xx", 2), "xx")
-    })
-    it("three characters with width of 1 returns characters separated by newline", function() {
-        assert.equal(wordWrap("xxx", 1), "x\nx\nx")
-    })
-    it("three characters with width of 2 returns 2 characters, newline, then 3rd character", function() {
-        assert.equal(wordWrap("xxx", 2), "xx\nx")
-    })
-    it("three characters with width of 3 returns the characters", function() {
-        assert.equal(wordWrap("xxx", 2), "xx\nx")
-    })
-    it("two characters separated by a space with width of 1 returns the characters separated by a newline", function() {
-        assert.equal(wordWrap("x x", 1), "x\nx")
-    })
-    it("two characters separated by a space with width of 2 returns the characters separated by a newline", function() {
-        assert.equal(wordWrap("x x", 2), "x\nx")
-    })
-    it("two characters separated by a space with width of 3 returns the characters separated by a space", function() {
-        assert.equal(wordWrap("x x", 3), "x x")
-    })
-    it("a variety of passing tests", function() {
-        assert.equal(wordWrap("x x x", 1), "x\nx\nx")
-        assert.equal(wordWrap("x x x", 2), "x\nx\nx")
-        assert.equal(wordWrap("x x x", 3), "x x\nx")
-        assert.equal(wordWrap("x x x", 4), "x x\nx")
-        assert.equal(wordWrap("x x x", 5), "x x x")
-        assert.equal(wordWrap("xx xx", 1), "x\nx\nx\nx")
-        assert.equal(wordWrap("xx xx", 2), "xx\nxx")
-        assert.equal(wordWrap("xx xx", 3), "xx\nxx") 
-    })
-    it("two pairs of characters separated by a space with width of 4 returns the pairs separated by a space", function() {
-        assert.equal(wordWrap("xx xx", 4), "xx\nxx")
-    })
-    it("more bunches of passing tests", function() {
-        assert.equal(wordWrap("xx xx", 5), "xx xx")
-        assert.equal(wordWrap("xx xx xx", 1), "x\nx\nx\nx\nx\nx")
-        assert.equal(wordWrap("xx xx xx", 2), "xx\nxx\nxx")
-        assert.equal(wordWrap("xx xx xx", 3), "xx\nxx\nxx")
-        assert.equal(wordWrap("xx xx xx", 4), "xx\nxx\nxx")
-        assert.equal(wordWrap("xx xx xx", 5), "xx xx\nxx")
-        assert.equal(wordWrap("xx xx xx", 6), "xx xx\nxx")
-        assert.equal(wordWrap("xx xx xx", 7), "xx xx\nxx")
-        assert.equal(wordWrap("xx xx xx", 8), "xx xx xx")
-    })
-
-
-})
-        
+// This is a Jest unit test - see https://jestjs.io/docs/en/getting-started for more information
+test('doAllTheThings should be true', () => {
+    expect(doAllTheThings()).toBeTruthy();
+}); 
